d6a97f9535f8d5e7e24f9e1e14d8c439f3cc516f
AnaIranzo <aeiranzom@gmail.com>
2023-02-23 09:57:49 +0100

    change data to parent

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,13 +9,7 @@ class Main extends Component {
     super(props)
     this.state = {
       
-        formNew: {
-          title:'',
-          abstract:'',
-          url:'',
-          author:'',
-          date:''
-        }
+      newsData: []
         
       
     }
@@ -30,18 +24,59 @@ class Main extends Component {
     const author = e.target.author.value;
     const date = e.target.date.value;
 
-    this.setState({...this.state, formNew: {title, abstract, url, author, date}})
+    const formNew = {
+      title,
+      abstract,
+      url,
+      author,
+      date
+    }
+
+    //this.setState({...this.state, formNew: {title, abstract, url, author, date}})
+    this.setState({newsData:[...this.state.newsData, formNew]})
+  }
+
+  getNews = async () => {
+    const topic = 'Health';
+    const response = await fetch(`https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=${process.env.REACT_APP_API_KEY_NEWS}`);
+    const data = await response.json();
+  
+    const dataResults = data.results;
+    const news = dataResults.slice(1,6);//quito la primera porque trae datos de admin o plantilla
+  
+    this.setState({newsData: news})
+    console.log(news);
   
   }
 
 
+
+
+  componentDidMount() { 
+    this.getNews();
+    
+  }
+  
+  componentDidUpdate(prevProps, prevState) { 
+    console.log("****DATOS DEL PASADO****");
+    console.log('prevProps: ', prevProps, 'prevState: ', prevState)
+    
+    console.log("****DATOS DEL PRESENTE****");
+    console.log("++PROPS++");
+   //
+    
+    console.log("++STATE++");
+    console.log(this.state);
+  
+    
+  } 
 
   render() {
     return <main>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/form" element={<Form onSubmitForm={this.handleForm}/>}/>
-        <Route path="/list" element={<ListNews data={this.state.formNew}/>}/>
+        <Route path="/list" element={<ListNews data={this.state.newsData}/>}/>
       </Routes>
     </main>;
   }
