caf44f18c2724d4c70a9fa58e8f329eb9467aae3
AnaIranzo <aeiranzom@gmail.com>
2023-02-23 16:43:33 +0100

    add context

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 
 import './App.css';
+import { useState } from "react";
 import Header from './components/Header'
 import Footer from './components/Footer';
 import Main from './components/Main';
 import { BrowserRouter } from 'react-router-dom';
+import { userContext } from "./context/userContext";
 
 function App() {
+  const [user, setUser] = useState(""); 
+
+  const login = (name) => setUser(name); 
+  const logout = () => setUser(""); 
+
+  const data = {
+    user,
+    login,
+    logout
+  }
+
   return (
     <>
     <BrowserRouter>
+    <userContext.Provider value={data}>
     <Header />
     <Main/>
+    </userContext.Provider>
     </BrowserRouter>
     <Footer/>
     </>
