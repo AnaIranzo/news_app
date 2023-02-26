e8e97ee1e204f26a65fa96073094c322f799af4d
AnaIranzo <aeiranzom@gmail.com>
2023-02-26 18:28:13 +0100

    add first styles

--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -1,10 +1,79 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import {  userContext} from "../../../context/userContext";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+
+import { v4 as uuidv4 } from 'uuid';
+
 
 class Nav extends Component {
+
+
+
+ 
   render() {
-    return <nav>
+    return (
+    
+      <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+         
+          
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Button
+                key={uuidv4()}
+                
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              ><Link to='/'>Home</Link></Button>
+               <Button
+                key={''}
+                
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              ><Link to='/form'>Form</Link></Button>
+               <Button
+                key={uuidv4()}
+                
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              ><Link to='/list'>List</Link> </Button>
+          </Box>
+          
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Button
+                key={uuidv4()}
+                
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              ><Link to='/'>Home</Link></Button>
+               <Button
+                key={''}
+                
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              ><Link to='/form'>Form</Link></Button>
+               <Button
+                key={uuidv4()}
+                
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              ><Link to='/list'>List</Link> </Button>
+          </Box>
+
+          <userContext.Consumer>
+        {({ logout, user }) => user ?
+          <span>Welcome, {user} <Button variant="contained" size="small" onClick={logout}>Logout</Button></span>
+          : ""
+        }
+      </userContext.Consumer>
+        </Toolbar>
+      </Container>
+    </AppBar>
+    
+    )
+    
+    
+   /*  <nav>
 
     <Link to='/'>Home</Link>
     <Link to='/form'>Form</Link>
@@ -17,7 +86,9 @@ class Nav extends Component {
         }
       </userContext.Consumer>
 
-    </nav>;
+    </nav>; */
+
+    
   }
 }
 
